b2039e0d8465c601eea666a463d474257960d83f
Brock Belmonte <brockbel@gmail.com>
2018-04-24 19:34:47 -0500

    This is the server.js file and the home page html

---
+++ server.js
@@ -0,0 +1,23 @@
+// Dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+
+// Creating an Express server
+var app = express();
+
+// Setting the initial port
+var PORT = process.env.PORT || 8080;
+
+// Data parsing for the Express app
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Linking to the route files
+require('./app/routing/apiRoutes')(app);
+require('./app/routing/htmlRoutes')(app);
+
+// Listening to start the server
+app.listen(PORT, function(){
+    console.log('App listening on PORT: ' + PORT);
+});
